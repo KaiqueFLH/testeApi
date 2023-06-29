12b6169faf830a4ef36e3c924013be2fd1f76730
KaiqueFLH <kaique_homem@estudante.sesisenai.org.br>
2023-06-28 21:37:21 -0300

    apiEntreAspasFuncionando

--- src/usuarios/controllers/usuario-controller.js
+++ src/usuarios/controllers/usuario-controller.js
@@ -2,43 +2,67 @@ const express = require("express");
 const routes = express.Router();
 const Usuario = require("./models/usuario");
 
-function createRoute(){
-    routes.post('/usuarios', async (req,res)=>{
-        console.log("Criado: ",req.body);
+function createRoute() {
+    routes.post('/usuarios', async (req, res) => {
+        console.log("Criado: ", req.body);
         await Usuario.create(req.body);
         res.json();
     })
 }
 
-function findAllRoute(){
-    routes.get('/usuarios', (req,res)=>{
-        res.json([]);
+function findAllRoute() {
+    routes.get('/usuarios',async (req, res) => {
+        const users = await Usuario.findAll();
+        console.log(users.every(user => user instanceof Usuario)); // true
+        console.log("All users:", JSON.stringify(users, null, 2));
+        res.json(await Usuario.findAll());
     })
 }
 
-function findByIdRoute(){
-    routes.get('/usuarios/:id', (req,res)=>{
+function findByIdRoute() {
+    routes.get('/usuarios/:id',async (req, res) => {
         console.log(req.params)
-        res.json();
+        const users = await Usuario.findAll({
+            where: {
+                id: req.params.id
+            }
+        });
+        console.log(users.every(user => user instanceof Usuario)); // true
+        console.log("User Finded:", JSON.stringify(users, null, 2));
+        res.json(await Usuario.findOne({
+            where:{
+                id:req.params.id
+            }
+        }));
     })
 }
 
-function updateRoute(){
-    routes.put('/usuarios', (req,res)=>{
+function updateRoute() {
+    routes.put('/usuarios', async (req, res) => {
         console.log(req.body);
+        await Usuario.update(req.body, {
+            where: {
+                id: req.body.id
+            }
+        });
         res.json();
     })
 }
 
-function removeRoute(){
-    routes.delete('/usuarios/:id', (req,res)=>{
-        console.log(req.params)
+function removeRoute() {
+    routes.delete('/usuarios/:id', async (req, res) => {
+        console.log(req.params);
+        await Usuario.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
         res.json();
     })
 }
 
 
-function registraUsuariosRotas(){
+function registraUsuariosRotas() {
     createRoute();
     findAllRoute();
     findByIdRoute();
